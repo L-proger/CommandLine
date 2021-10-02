5184b78ed36ad74e7cec27c25162eb0b8bf79693
l-proger <l-proger@yandex.ru>
2021-10-03 02:39:15 +0300

    Add bool value converter

--- Src/CommandLine/ValueConverter.h
+++ Src/CommandLine/ValueConverter.h
@@ -4,31 +4,38 @@
 #include <string>
 
 namespace CommandLine {
-    template<typename T, bool isIntegral = std::is_integral_v<T>>
+    template<typename T>
     class ValueConverter {
     public:
-        static T convert(const std::string& /*value*/) {
-            throw ParserException("ValueConverter: converter not found for type T");
-        }
-    };
-
-    template<typename T>
-    class ValueConverter<T, true> {
-    public:
         static T convert(const std::string& value) {
-            if constexpr(std::is_signed_v<T>){
+            if constexpr (std::is_integral_v<T> && std::is_signed_v<T>) {
                 auto result = std::strtoll(value.c_str(), nullptr, 0);
-                if((result < (std::numeric_limits<T>::min)()) || (result > (std::numeric_limits<T>::max)())){
-                    throw ParserException("Value is out of range");
-                }
-                return static_cast<T>(result);
-            }else{
-                auto result = std::strtoull(value.c_str(), nullptr, 0);
-                if((result < (std::numeric_limits<T>::min)()) || (result > (std::numeric_limits<T>::max)())){
+                if ((result < (std::numeric_limits<T>::min)()) || (result > (std::numeric_limits<T>::max)())) {
                     throw ParserException("Value is out of range");
                 }
                 return static_cast<T>(result);
             }
+            else  if constexpr (std::is_integral_v<T> && !std::is_signed_v<T>) {
+                auto result = std::strtoull(value.c_str(), nullptr, 0);
+                if ((result < (std::numeric_limits<T>::min)()) || (result > (std::numeric_limits<T>::max)())) {
+                    throw ParserException("Value is out of range");
+                }
+                return static_cast<T>(result);
+            }
+            else {
+                throw ParserException("ValueConverter: converter not found for type T");
+            }
+            
         }
     };
+
+    template<>
+    class ValueConverter<bool> {
+    public:
+        static bool convert(const std::string& value) {
+            return (value == "true") || (value == "True") || (value == "TRUE");
+        }
+    };
+
+
 }
